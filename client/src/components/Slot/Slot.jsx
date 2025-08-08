6ce39e6bac071e159a37dc1193498ee52539ba0b
Oleksandr Rad <clavigo365@gmail.com>
2025-08-08 13:04:53 +0300

    feat: add the ability to press the button and spin the slot

--- client/src/components/Slot/Slot.jsx
+++ client/src/components/Slot/Slot.jsx
@@ -1,26 +1,51 @@
 import { useState } from "react";
+import classnames from "classnames";
 import styles from "./Slot.module.scss";
 
 export const Slot = () => {
   const [isSpinning, setIsSpinning] = useState(false);
 
+  const handleSpinClick = () => {
+    isSpinning ? setIsSpinning(false) : setIsSpinning(true);
+  };
+
   return (
     <div className={styles.slot}>
       <div className={styles.slot__container}>
         <div className={styles.slot__item}>
-          <span className={styles.slot__spinner}>x</span>
+          <span
+            className={classnames(styles.slot__spinner, {
+              [styles["slot__spinner-active"]]: isSpinning,
+            })}
+          >
+            x
+          </span>
         </div>
 
         <div className={styles.slot__item}>
-          <span className={styles.slot__spinner}>x</span>
+          <span
+            className={classnames(styles.slot__spinner, {
+              [styles["slot__spinner-active"]]: isSpinning,
+            })}
+          >
+            x
+          </span>
         </div>
 
         <div className={styles.slot__item}>
-          <span className={styles.slot__spinner}>x</span>
+          <span
+            className={classnames(styles.slot__spinner, {
+              [styles["slot__spinner-active"]]: isSpinning,
+            })}
+          >
+            x
+          </span>
         </div>
       </div>
 
-      <button className={styles.slot__button}>Spin!</button>
+      <button className={styles.slot__button} onClick={handleSpinClick}>
+        Spin!
+      </button>
     </div>
   );
 };
